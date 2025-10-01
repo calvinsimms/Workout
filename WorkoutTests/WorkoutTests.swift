9ba6ff5ea1e2dbfa7efc24ee1584a91412692a56
Calvin Simms <calvinsimms0@gmail.com>
2025-10-01 19:21:40 -0400

    Initial Commit

---
+++ WorkoutTests/WorkoutTests.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutTests.swift
+//  WorkoutTests
+//
+//  Created by Calvin Simms on 2025-10-01.
+//
+
+import Testing
+@testable import Workout
+
+struct WorkoutTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
