8fad979f34de1b44d579f074989ad9567f43d4b1
Calvin <calvinsimms0@gmail.com>
2025-10-01 22:02:39 -0400

    gitignore

--- Workout/ContentView.swift
+++ Workout/ContentView.swift
@@ -11,37 +11,90 @@ import SwiftData
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var items: [Item]
-
-    var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
+    @Model
+    final class Item {
+        var title: String
+        
+        init(title: String) {
+            self.title = title
         }
     }
 
+    var body: some View {
+        ZStack {
+            Color("Background")
+                .ignoresSafeArea()
+            
+            VStack{
+                
+                HStack {
+                    Button(action: {
+                        // Add action
+                    }) {
+                        Text("Edit")
+                            .font(.system(.title3, weight: .bold))
+                            .padding(10)
+                            .background(Color("Button"))
+                            .foregroundColor(.black)
+                            .cornerRadius(30)
+                    }
+                    Spacer()
+                    Button(action: addItem) {
+                        Image(systemName: "plus")
+                            .font(.title)
+                            .foregroundColor(.black)
+                            .padding(10)
+                            .background(Color("Button"))
+                            .clipShape(Circle())
+                    }
+                }
+                .padding(.horizontal, 20)
+                
+                // Header
+                HStack {
+                    Text("Workout")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .padding(.horizontal, 20)
+                        .padding(.vertical, 20)
+                        .foregroundColor(.black)
+                    Spacer()
+                }
+                
+                List {
+                    ForEach(items) { item in
+                        NavigationLink {
+                            Text("Workout: \(item.title)")
+                        } label: {
+                            HStack {
+                                Text(item.title)
+                                    .font(.system(.title, weight: .bold))
+                                    .foregroundColor(.black)
+                                    .padding(.horizontal, 20)
+                                Spacer()
+                            }
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .padding(.vertical, 20)
+                            .background(Color("Background"))
+                            
+                        }
+                        .listRowBackground(Color("Background"))
+                        .listRowSeparatorTint(.gray)
+                        .listRowSeparator(.visible, edges: .all) // Show separators
+                        
+                    }
+                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                }
+                .listStyle(.plain)
+                
+                Spacer()
+            }
+        }
+    }
+    
     private func addItem() {
         withAnimation {
-            let newItem = Item(timestamp: Date())
+            let newItem = Item(title: "New Workout")
             modelContext.insert(newItem)
         }
     }
